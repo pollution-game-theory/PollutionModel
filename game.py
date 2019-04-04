92bfee674952829bf7a1560bcaebaaee915e0c37
StephendeRiel <48258651+StephendeRiel@users.noreply.github.com>
2019-04-04 13:56:00 -0400

    added explanation for variables

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 from resource import Resource
 
 class Game:
+    #value 1 = the cost, value 2 = the damage/pollution, value 3 = the value to the countries (i.e. how much it fills their need)
     r1 = Resource(10,2,5)
     r2 = Resource(5,5,5)
     r3 = Resource(2,10,5) 
     totalpollution = 0
     gametime = 0
+    #value 1 = budget, value 2 = need, value 3 = growth
     developing = Country(1000, 200, 100)
     developed = Country(100000, 20000, 10000)
     
