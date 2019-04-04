caba9787ecb625f83649ce726fb846082fe36906
StephendeRiel <48258651+StephendeRiel@users.noreply.github.com>
2019-04-04 13:32:40 -0400

    added classes

---
+++ game.py
@@ -0,0 +1,9 @@
+from resource import Resource
+
+class Game:
+    r1 = Resource(10,2)
+    r2 = Resource(5,5)
+    r3 = Resource(2,10) 
+    totalpollution = 0
+    gametime = 0
+    
---
+++ resource.py
@@ -0,0 +1,5 @@
+class Resource:
+    
+    def __init__(self, cost, damage):
+        self.cost = cost
+        self.damage = damage
