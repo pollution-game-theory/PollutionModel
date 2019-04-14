22ad999461b248ddee50f04df81ef6cd84845689
StephendeRiel <48258651+StephendeRiel@users.noreply.github.com>
2019-04-14 14:07:26 -0400

    Added Micheal's additions to Taxman and Game

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import random
-from resource import Resource
 from country import Country
+from resource import Resource
+from taxman import Taxman
 
 class Game:
     #resource variables
@@ -16,49 +17,91 @@ class Game:
     #the budget is the total money available for use in the country
     #the need is how much value they need per round (i.e. a country with a need of 200 would need 100 of a resource with the value 2 per round.
     #the growth is a steady value that gets added to the budget every round.
-    def __init__ (self,
-                  #r1 = coal
-                  #r2 = electricity
-                  #r3 = nuclear
-                  r1 = Resource(2,8,5,2,1,10),
-                  r2 = Resource(8,2,5,0,1,100),
-                  r3 = Resource(2,2,8,10,1,1000),
-                 Agents = [Country(1000, 200, 100),Country(100000, 20000, 10000)]):
+    
+    #r1 = coal
+    #r2 = electricity
+    #r3 = nuclear
+    def __init__(self,
+                  r1 = Resource(40,40,60,4,1,0),
+                  r2 = Resource(80,10,40,0,1,0),
+                  r3 = Resource(60,20,80,10,1,0),
+                  Agents = [Country(100000, 8000, 10000),Country(1000000, 80000, 100000)],
+                  Taxman = Taxman(),
+                  total_pollution = 0,
+                  total_risk = 0,
+                  catastrophes = 0,
+                  gametime = 0,
+                  global_collapse = False,
+                  country_collapse = False,
+                  threshold = 1000000):
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
         self.rl = [r1, r2, r3]
-        self.total_pollution = 0
-        self.total_risk = 0
-        self.catastrophes = 0
-        self.gametime = 0
+        self.names = ["Coal" ,"Wind/Solar", "Nuclear"]
+        self.total_pollution = total_pollution
+        self.total_risk = total_risk
+        self.catastrophes = catastrophes
+        self.gametime = gametime
         self.developing = Agents[0]
         self.developed = Agents[1]
-        self.threshold = 100000
+        self.Taxman = Taxman
+        self.threshold = threshold
+        self.global_collapse = global_collapse
+        self.country_collapse = country_collapse
+        self.currentResPrices = [self.r1.value/(self.r1.cost + .01),self.r2.value/(self.r2.cost+.01),self.r3.value/(self.r3.cost+.01)]
     
     def take_turn(self, country):
-        rrc = random.choice(self.rl)
+        self.country = country
+        self.currentResPrices = [self.r1.value/(self.r1.cost),self.r2.value/(self.r2.cost+.01),self.r3.value/(self.r3.cost+.01)]
+        #rrc now stands for round-resource-choice
+        rrc = self.rl[self.currentResPrices.index(max(self.currentResPrices))]
         if rrc.unlock == 1:
-            if rrc.unlockcost > country.budget:
+            if rrc.unlockcost > self.country.budget:
                 print ("country can't pay for this resource")
                 return
-            country.budget -= rrc.unlockcost
+            self.country.budget -= rrc.unlockcost
             rrc.unlock = 0
-        x = country.need
-        country.budget += country.growth
+        x = self.country.need
+        self.country.budget += self.country.growth
         
         self.total_risk += rrc.crisisRisk
-        if random.randint(0,100)< self.total_risk:
-            self.total_pollution +=40
+        if random.randint(0,200)< self.total_risk:
+            self.total_pollution +=5000
             self.total_risk += -10
             self.catastrophes += 1
             
-        while x > -1 and country.budget > 0:
-            country.budget -= rrc.cost
+        while x > -1 and self.country.budget > 0:
+            self.country.budget -= rrc.cost
             x -= rrc.value
             self.total_pollution += rrc.damage
         if x > 0:
-            print("")
-            print("country has failed, no money left")
+            self.country_collapse = True
+        if self.total_pollution >= self.threshold:
+            self.global_collapse = True
     
     def play_round(self):
+        if self.total_pollution >= self.Taxman.interventionThreshold:
+            if self.Taxman.interventionInProgress == False:
+                self.Taxman.interventionTally += 1
+                self.Taxman.interventionInProgress = True
+            if self.Taxman.interventionStage < self.Taxman.interventionLength and self.Taxman.interventionStage % self.Taxman.interventionFrequency == 0:
+                self.r1.cost += self.Taxman.interventionIntensity * 5
+                self.r2.cost -= self.Taxman.interventionIntensity * 5
+                if self.r2.cost == 0:
+                    self.r2.cost = 10
+                self.Taxman.interventionStage += 1
+            elif self.Taxman.interventionStage < self.Taxman.interventionLength:
+                self.Taxman.interventionStage += 1
+            else:
+                self.Taxman.interventionStage = 0
+                self.Taxman.interventionThreshold = self.total_pollution + self.Taxman.thresholdIncreaseRate
+                if self.Taxman.reset == True:
+                    self.r1.cost = 40
+                    self.r2.cost = 80
+                self.Taxman.interventionInProgress = False
+            
+                
         self.take_turn(self.developing)
         self.take_turn(self.developed)
         self.gametime += 1
@@ -66,16 +109,25 @@ class Game:
     def fix_pollution(self, cost):
         self.total_pollution -= cost*100
         
+    def playgame(self):
+        
+        while self.global_collapse == False and self.country_collapse == False:
+            self.play_round()
+            print(["Current Resource Price: " + str(self.currentResPrices)])
+            print(["Length of Game: " + str(self.gametime)])
+            print(["Total Interventions: " + str(self.Taxman.interventionTally)])
+            print(["Player1 budget: " + str(self.developing.budget)])
+            print(["Player2 budget: "+ str(self.developed.budget)])
+            print(["Total Global Pollution: " + str(self.total_pollution) + "/" + str(self.threshold)])
+            print(["Current Total Global Risk: "+ str(self.total_risk)])
+            print(["Total Global Crises :"+ str(self.catastrophes)])
+            print("")
+        if self.country_collapse == True:
+            print("")
+            print("Game Over: Country Collapse due to Insufficient Funds!")
+        if self.global_collapse == True:
+            print("")
+            print("Game Over: Global Collapse!")
+
 x = Game()
-
-while x.total_pollution < x.threshold:
-    x.play_round()
-    print(["Total Pollution: ", x.total_pollution])
-    print(["Length of Game: ", x.gametime])
-    print(["Player1 budget: ", x.developing.budget])
-    print(["Player2 budget: ",x.developed.budget])
-    print(["Total Pollution: ", x.total_pollution])
-    print(["Total Risk: ",x.total_risk])
-    print(["Total Catastrophes :", x.catastrophes])
-    print("")
-
+x.playgame()
---
+++ taxman.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Taxman:
+    def __init__(self, interventionThreshold = 500000, interventionIntensity=2, interventionFrequency=2, interventionLength= 10, thresholdIncreaseRate = 100000, reset = False):
+        self.interventionThreshold = interventionThreshold #pollution level that initiates intervention
+        self.interventionIntensity = interventionIntensity #scale of 1-3 how much to tweak prices
+        self.interventionFrequency = interventionFrequency #the ratio of rounds-since-the-start-of-intervention: # of tweaks to the prices
+        self.interventionLength = interventionLength #how many rounds the intervention lasts
+        self.thresholdIncreaseRate = thresholdIncreaseRate # how to update the threshold after the intervention
+        self.reset = reset # whether the prices reset at the end of the intervention
+        self.interventionStage = 0 # of rounds since the intervention began
+        self.interventionInProgress = False
+        self.interventionTally = 0 # of interventions this Taxman has performed
+
